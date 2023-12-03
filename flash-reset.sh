077b6041b7cfde3c80df480253675942dee05725
schottj <jschott780@gmail.com>
2023-12-03 15:14:11 -0500

    builds

--- flash-reset.sh
+++ flash-reset.sh
@@ -7,7 +7,7 @@ esptool.py -p /dev/cu.usbserial-1410 \
 write_flash \
 --flash_mode dio \
 --flash_size 4MB \
---flash_freq 40m \
-0x1000 ../build/bootloader/bootloader.bin 0x8000 \
-../build/partition_table/partition-table.bin 0x10000 \
-../build/micropython.bin
+--flash_freq 80m \
+0x0 ../build/bootloader/bootloader.bin \
+0x8000 ../build/partition_table/partition-table.bin \
+0x10000 ../build/micropython.bin
